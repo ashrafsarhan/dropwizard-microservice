7c75b5b7c3a3ea9a79892061d165ac3a22273811
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2017-10-03 14:08:16 +0200

    Creating a running dropwizard version

--- src/main/java/com/dropwiz/ws/DropwizApplication.java
+++ src/main/java/com/dropwiz/ws/DropwizApplication.java
@@ -6,24 +6,29 @@ import io.dropwizard.setup.Environment;
 
 public class DropwizApplication extends Application<DropwizConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new DropwizApplication().run(args);
-    }
+	public static void main(final String[] args) throws Exception {
+		new DropwizApplication().run(args);
+	}
 
-    @Override
-    public String getName() {
-        return "dropwiz ws";
-    }
+	@Override
+	public String getName() {
+		return "dropwiz ws";
+	}
 
-    @Override
-    public void initialize(final Bootstrap<DropwizConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+	@Override
+	public void initialize(final Bootstrap<DropwizConfiguration> bootstrap) {
+		// TODO: application initialization
+	}
 
-    @Override
-    public void run(final DropwizConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+	@Override
+	public void run(final DropwizConfiguration configuration, final Environment environment) {
+		// TODO: implement application
+		final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(),
+				configuration.getDefaultName());
+		environment.jersey().register(resource);
+
+		final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+		environment.healthChecks().register("template", healthCheck);
+	}
 
 }
--- src/main/java/com/dropwiz/ws/DropwizConfiguration.java
+++ src/main/java/com/dropwiz/ws/DropwizConfiguration.java
@@ -1,10 +1,38 @@
 package com.dropwiz.ws;
 
-import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+
+import io.dropwizard.Configuration;
 
 public class DropwizConfiguration extends Configuration {
+	
     // TODO: implement service configuration
+	@NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) {
+        this.defaultName = name;
+    }
+    
 }
